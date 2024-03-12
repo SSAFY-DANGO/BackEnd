58bf3e51c7bb9cf0648e9c4fa409953c2a4e9f9d
On Yoo <dhsdb02@gmail.com>
2024-03-12 14:46:26 +0900

    fix(user): #16 토큰을 통해 유저를 찾는 메서드에 오타

--- backend/src/main/java/com/dango/dango/domain/user/service/UserService.java
+++ backend/src/main/java/com/dango/dango/domain/user/service/UserService.java
@@ -5,7 +5,7 @@ import com.dango.dango.domain.user.entity.User;
 public interface UserService {
 	User findUser(Long id);
 	User findUserByUsername(String username);
-	User findUserByToken()
+	User findUserByToken();
 	User addUser(User user);
 	User modifyUserNickName(Long id,String nickname);
 	User modifyUserPassword(Long id,String password);
