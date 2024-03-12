783816fa7012dc11305d4783e989d833b392c326
On Yoo <dhsdb02@gmail.com>
2024-03-12 14:19:43 +0900

    feat(user): #16 닉네임,이메일 존재여부 확인하는 메서드 추가

--- backend/src/main/java/com/dango/dango/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/dango/dango/domain/user/repository/UserRepository.java
@@ -10,4 +10,6 @@ import com.dango.dango.domain.user.entity.User;
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
 	Optional<User> findByUsername(String username);
+	boolean existsByUsername(String username);
+	boolean existsByNickname(String nickname);
 }
