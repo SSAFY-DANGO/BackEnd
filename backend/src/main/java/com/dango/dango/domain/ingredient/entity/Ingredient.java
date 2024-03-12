697a2f53f6cbfdb64c6ae8e8922e16cb937e30ba
유호영 <yhu2121@naver.com>
2024-03-12 18:06:48 +0900

    feat(ingredient): #5 ingredient 엔티티 작성 완료

--- backend/src/main/java/com/dango/dango/domain/ingredient/entity/Ingredient.java
+++ backend/src/main/java/com/dango/dango/domain/ingredient/entity/Ingredient.java
@@ -1,29 +1,58 @@
 package com.dango.dango.domain.ingredient.entity;
 
 import jakarta.persistence.*;
-import lombok.NoArgsConstructor;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Entity
-@NoArgsConstructor
-
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+@Getter
+@Table(name = "ingredient")
 public class Ingredient {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @ManyToOne
-    @Column(name = "refrigerator_id")
-    @JoinColumn(name ="id" )
+    @Column(name = "refrigerator_id", nullable = false)
+    @JoinColumn(name = "id")
     private Refrigerator refrigeratorId;
+
+    @NotNull
     private String name;
+    @NotNull
     private String category;
+
+    @NotNull
+    @Column(name = "expiration_time")
+    @CreationTimestamp // 이후 수정 필요 -> 소비기한
     private LocalDateTime expirationTime;
+
+    @NotNull
+    @Column(name = "update_time")
+    @CreationTimestamp
+    @UpdateTimestamp
     private LocalDateTime updateTime;
+
+    @NotNull
+    @Column(name = "create_time")
+    @CreationTimestamp
     private LocalDateTime createTime;
+
+    @NotNull
+    @Column(name = "input_time")
+    @CreationTimestamp
     private LocalDateTime inputTime;
+
+    @Column(name = "output_time")
     private LocalDateTime outputTime;
+
     private int type;
     private boolean exist;
 
