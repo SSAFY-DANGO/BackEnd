406af7be5256300ea39f459e726bea41ee7e1be0
On Yoo <dhsdb02@gmail.com>
2024-03-14 09:35:31 +0900

    fix(user): string 형태의 key를 삭제하고 key 객체를 추가
    string으로key를 만드는 과정이 deprecated 되어 해당 부분 수정하였습니다

--- backend/src/main/java/com/dango/dango/global/common/util/JwtTokenUtil.java
+++ backend/src/main/java/com/dango/dango/global/common/util/JwtTokenUtil.java
@@ -1,25 +1,50 @@
 package com.dango.dango.global.common.util;
 
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
 import java.util.Date;
+
+import javax.management.RuntimeErrorException;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import com.dango.dango.domain.user.entity.User;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
+@Component
 public class JwtTokenUtil {
 	private static final long ACCESS_TOKEN_VALID_PERIOD = 1000L * 60 * 60 * 24 * 8;
 
-	public static String createToken(User user,String key){
+	static private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
+
+	public static String createToken(User user){
 		Claims claims = Jwts.claims();
 		claims.put("username",user.getUsername());
 
 		return Jwts.builder()
 			.setClaims(claims)
 			.setIssuedAt(new Date(System.currentTimeMillis()))
-			.setExpiration(new Date(System.currentTimeMillis()+ACCESS_TOKEN_VALID_PERIOD))
-			.signWith(SignatureAlgorithm.HS256,key)
+			.setExpiration(new Date(System.currentTimeMillis()+ ACCESS_TOKEN_VALID_PERIOD))
+			.signWith(key)
 			.compact();
 	}
+	public static String createRefreshToken(){
+		Claims claims = Jwts.claims();
+		return Jwts.builder()
+			.setClaims(claims)
+			.setIssuedAt(new Date(System.currentTimeMillis()))
+			.setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALID_PERIOD))
+			.signWith(key)
+			.compact();
+	}
+
+
+
 }
