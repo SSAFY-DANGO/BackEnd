14d88bd641055131a29a852fc933a956ea6f16d5
On Yoo <dhsdb02@gmail.com>
2024-03-14 11:00:02 +0900

    fix(user): filter에 있던 token 관련 메서드를 관리 유틸로 이동
    - filter에 있던 메서드들을 util로 옮겼습니다.

--- backend/src/main/java/com/dango/dango/global/common/util/JwtTokenUtil.java
+++ backend/src/main/java/com/dango/dango/global/common/util/JwtTokenUtil.java
@@ -15,6 +15,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -24,18 +25,19 @@ public class JwtTokenUtil {
 
 	static private Key key = Keys.secretKeyFor(SignatureAlgorithm.HS256);
 
-	public static String createToken(User user){
+	public static String createToken(User user) {
 		Claims claims = Jwts.claims();
-		claims.put("username",user.getUsername());
+		claims.put("username", user.getUsername());
 
 		return Jwts.builder()
 			.setClaims(claims)
 			.setIssuedAt(new Date(System.currentTimeMillis()))
-			.setExpiration(new Date(System.currentTimeMillis()+ ACCESS_TOKEN_VALID_PERIOD))
+			.setExpiration(new Date(System.currentTimeMillis() + ACCESS_TOKEN_VALID_PERIOD))
 			.signWith(key)
 			.compact();
 	}
-	public static String createRefreshToken(){
+
+	public static String createRefreshToken() {
 		Claims claims = Jwts.claims();
 		return Jwts.builder()
 			.setClaims(claims)
@@ -45,6 +47,29 @@ public class JwtTokenUtil {
 			.compact();
 	}
 
+	public static String extractToken(HttpServletRequest request) {
+		String bearerToken = request.getHeader("Authorization");
+		if (bearerToken != null && bearerToken.startsWith("Bearer"))
+			return bearerToken.substring(7);
+		return null;
+	}
 
+	public static boolean validateToken(String token) {
+		try {
+			Claims claims = Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+			Date expiration = claims.getExpiration();
 
+			if (expiration.before(new Date())) {
+				// 액세스 토큰이 만료되었을 경우
+				throw new IllegalArgumentException("토큰이 만료되었습니다");
+			}
+		} catch (Exception e) {
+			return false;
+		}
+		return true;
+	}
+
+	public static Claims extractClaims(String token) {
+		return Jwts.parser().setSigningKey(key).parseClaimsJws(token).getBody();
+	}
 }
