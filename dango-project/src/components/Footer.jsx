995019720807853c77338221898bf5fca5894a11
전예은 <ye_eun12@naver.com>
2024-03-19 17:56:48 +0900

    Footer 아이콘 가운데 정렬 완료

--- dango-project/src/components/Footer.jsx
+++ dango-project/src/components/Footer.jsx
@@ -2,18 +2,23 @@ import React from 'react';
 import CartFilledIcon from '../../public/cartFilledIcon.svg';
 import FridgeIcon from '../../public/fridgeIcon.svg';
 import MypageEmptyIcon from '../../public/mypageEmptyIcon.svg';
+import '../styles/Footer.css';
 
 export default function Footer() {
   return (
     <>
       <div>Footer</div>
-      <div className='flex justify-center items-center'>
-        <div className=''>
+      <footer className='fixed bottom-0 left-0 right-0 h-11 overflow-hidden border-t-2 border-solid'>
+        <div>
           <img src={CartFilledIcon} />
+        </div>
+        <div>
           <img src={FridgeIcon} />
+        </div>
+        <div>
           <img src={MypageEmptyIcon} />
         </div>
-      </div>
+      </footer>
     </>
   );
 }
