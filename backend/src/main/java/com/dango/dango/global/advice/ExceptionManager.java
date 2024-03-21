18cab28989037d1cad076dcebac95ea3449a5d97
On Yoo <dhsdb02@gmail.com>
2024-03-21 09:08:36 +0900

    feat(user): #23 토큰이 없을때 예외처리 추가

--- backend/src/main/java/com/dango/dango/global/advice/ExceptionManager.java
+++ backend/src/main/java/com/dango/dango/global/advice/ExceptionManager.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.dango.dango.global.common.response.ErrorResponse;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @RestControllerAdvice
 public class ExceptionManager {
 
@@ -29,6 +31,12 @@ public class ExceptionManager {
 
 	@ExceptionHandler(IllegalArgumentException.class)
 	public ResponseEntity<ErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex){
+		return ResponseEntity.ok(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
+	}
+
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<ErrorResponse> handleEntityNotFoundException(EntityNotFoundException ex){
 		return ResponseEntity.ok(new ErrorResponse(HttpStatus.BAD_REQUEST.value(), ex.getMessage()));
 	}
 
