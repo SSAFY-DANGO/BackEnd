9bf62749620b951a51468296436444049e311053
전예은 <ye_eun12@naver.com>
2024-03-21 12:56:08 +0900

    feat: Axios 연결 at Login and Signup Page

--- dango-project/src/pages/Login.jsx
+++ dango-project/src/pages/Login.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import '../styles/Landing.css';
 import '../styles/Common.css';
+// import CommonButton from '../components/CommonButton'
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { RiKakaoTalkFill } from 'react-icons/ri';
+import { loginUser } from '../api/Api';
 
 export default function Login() {
   const navigate = useNavigate();
+
+  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      const response = await loginUser({ email, password });
+      console.log('로그인 성공', response);
+    } catch (error) {
+      console.log('로그인 실패', error);
+    }
+  };
 
   const navigateSignup = () => {
     navigate('/sign-up');
@@ -13,47 +27,45 @@ export default function Login() {
 
   return (
     <>
-      <div className=''>
-        <div className='landing-background '>
-          <div className='flex flex-col justify-center items-center '>
-            <div className='text-5xl font-bold pb-10 font-title'>
-              당신의 냉장고, 당고
-            </div>
-
-            <div className='text-3xl pb-10 font-aggroS'>문을 열어 주세요</div>
-            <div className='m-4 w-3/4 flex justify-center flex-col'>
-              <div>이메일</div>
-              <input
-                placeholder='ssafy@gmail.com'
-                className='rounded-lg p-3 w-full'
-              />
-            </div>
-            <div className='m-4 w-3/4 flex justify-center flex-col'>
-              <div>비밀번호</div>
-              <input placeholder='********' className='rounded-lg p-3 w-full' />
-            </div>
-            <div className='w-3/4 flex justify-center'>
-              <button className='long-thick-button'>
-                <RiKakaoTalkFill
-                  color='#3A1D1D'
-                  className='inline-block align-top translate-y-1 mr-1'
-                />
-                <span>카카오 로그인</span>
-              </button>
-            </div>
-            <div className='w-3/4 flex justify-center'>
-              <button className='long-thick-button'>로그인</button>
-            </div>
-            <div className='w-3/4 flex justify-center'>
-              <button
-                className='long-thick-button'
-                onClick={() => navigateSignup()}
-              >
-                회원가입
-              </button>
-            </div>
-          </div>
+      <div className='landing-background flex flex-col justify-center items-center h-[150vh]'>
+        <div className='text-7xl font-bold '>당신의 냉장고, 당고</div>
+        <div className='text-3xl'>문을 열어 주세요</div>
+        <div className='m-4 w-3/4 flex justify-center flex-col'>
+          <div>이메일</div>
+          <input
+            placeholder='ssafy@gmail.com'
+            className='rounded-lg p-3 w-full'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
+        <div className='m-4 w-3/4 flex justify-center flex-col'>
+          <div>비밀번호</div>
+          <input
+            placeholder='********'
+            className='rounded-lg p-3 w-full'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        {/* <div className='w-[90%] flex-row justify-center'>
+          <div>이메일 주소</div>
+          <input
+            className='common-input'
+            placeholder=' ssafy@email.com'
+          ></input>
+        </div> */}
+        {/* <div className='w-[90%] flex-row justify-center'>
+          비밀번호
+          <div></div>
+          <input className='common-input' placeholder=' *********'></input>
+        </div> */}
+        <button className='long-thick-button' onClick={navigateSignup}>
+          회원가입
+        </button>
+        <button className='long-thick-button' onClick={handleLogin}>
+          로그인
+        </button>
       </div>
     </>
   );
--- dango-project/src/pages/SignUp.jsx
+++ dango-project/src/pages/SignUp.jsx
@@ -1,29 +1,78 @@
 import React from 'react';
+import '../styles/Landing.css';
+import '../styles/Common.css';
 import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
+import { signUpUser } from '../api/Api';
 
 export default function SignUp() {
   const navigate = useNavigate();
 
+  const [nickname, setNickname] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = async () => {
+    console.log('닉네임', nickname);
+
+    try {
+      const response = await signUpUser({ nickname, email, password });
+      console.log('로그인 성공', response);
+      navigateConnectFridge();
+    } catch (error) {
+      console.log('로그인 실패', error);
+    }
+  };
+
+  const navigateLogin = () => {
+    navigate('/');
+  };
   const navigateConnectFridge = () => {
     navigate('/connect-fridge');
   };
   return (
     <>
-      <div className='landing-background flex flex-col justify-center items-center h-screen'>
+      <div className='flex justify-center items-center h-[100vh] landing-background'>
         <div
           className='text-5xl font-bold font-noto-sans-kr p-10'
           style={{ position: 'absolute', top: '20px', left: '20px' }}
         >
           {' '}
-          &lt; 회원가입{' '}
+          <button onClick={navigateLogin}>&lt;</button> 회원가입{' '}
         </div>
-        <form className='text-3xl'>
-          <div className='p-10'>
-            <div>이메일 주소</div>
-            <input />
+        {/* 형식 form - requestbody임으로 submit 불가 */}
+        <form className='w-full ml-[14vw]'>
+          <div className='m-4 w-3/4 flex justify-center flex-col mb-10'>
+            <div>닉네임</div>
+            <input
+              placeholder='김싸피'
+              className='rounded-lg p-3 w-full'
+              value={nickname}
+              onChange={(e) => setNickname(e.target.value)}
+            />
           </div>
-
-          <button onClick={navigateConnectFridge}> 가입하기 </button>
+          <div className='m-4 w-3/4 flex justify-center flex-col mb-10'>
+            <div>이메일 주소</div>
+            <input
+              placeholder='ssafy@gmail.com'
+              className='rounded-lg p-3 w-full'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className='m-4 w-3/4 flex justify-center flex-col mb-10'>
+            <div>비밀번호</div>
+            <input
+              placeholder='**********'
+              className='rounded-lg p-3 w-full'
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button onClick={handleSignUp} className='long-thick-button'>
+            {' '}
+            가입하기{' '}
+          </button>
         </form>
       </div>
     </>
