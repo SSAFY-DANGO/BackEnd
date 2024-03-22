f683892c32500d319eeb89ddf307a4e235f8015a
On Yoo <dhsdb02@gmail.com>
2024-03-22 17:39:58 +0900

    feat(user): #31 유저 정보 조회 컨트롤러 추가

--- backend/src/main/java/com/dango/dango/domain/user/controller/UserController.java
+++ backend/src/main/java/com/dango/dango/domain/user/controller/UserController.java
@@ -2,18 +2,22 @@ package com.dango.dango.domain.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dango.dango.domain.user.dto.UserLoginRequest;
 import com.dango.dango.domain.user.dto.UserLoginResponse;
+import com.dango.dango.domain.user.dto.UserProfileResponse;
 import com.dango.dango.domain.user.dto.UserRegistrationRequest;
 import com.dango.dango.domain.user.dto.UserRegistrationResponse;
 import com.dango.dango.domain.user.service.AuthService;
 import com.dango.dango.domain.user.service.RegistrationService;
+import com.dango.dango.domain.user.service.UserService;
 import com.dango.dango.global.common.response.CustomResponse;
 
 import jakarta.validation.Valid;
@@ -21,10 +25,11 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/user")
+@RequestMapping("/api/users")
 public class UserController {
 	private final RegistrationService registrationService;
 	private final AuthService authService;
+	private final UserService userService;
 
 	@PostMapping("/login")
 	public CustomResponse<UserLoginResponse> login(@RequestBody @Valid UserLoginRequest userLoginRequest){
@@ -42,6 +47,13 @@ public class UserController {
 	public CustomResponse<String> logout(@RequestHeader("Authorization")String accessToken,@RequestHeader("Refresh-Token")String refreshToken){
 		authService.logout(accessToken, refreshToken);
 		return new CustomResponse<>(HttpStatus.OK.value(),"","logout 성공!");
+	}
+	
+
+	@GetMapping("/profile")
+	public CustomResponse<UserProfileResponse> profile(){
+		UserProfileResponse userProfileResponse = userService.getProfile();
+		return new CustomResponse<>(HttpStatus.OK.value(),userProfileResponse,"유저 정보 조회 성공");
 	}
 
 }
