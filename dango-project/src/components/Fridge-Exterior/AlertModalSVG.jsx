8ef20c73c16014a2f33228b8b0d30a9f874266df
전예은 <ye_eun12@naver.com>
2024-03-22 17:42:01 +0900

    feat: FridgeExterior 및 모달 구현

---
+++ dango-project/src/components/Fridge-Exterior/AlertModalSVG.jsx
@@ -0,0 +1,56 @@
+import React from 'react';
+
+export default function AlertModalSVG() {
+  return (
+    <>
+      <svg
+        width='auto'
+        height='auto'
+        viewBox='0 0 146 217'
+        fill='none'
+        xmlns='http://www.w3.org/2000/svg'
+      >
+        <rect
+          width='145.349'
+          height='215.509'
+          transform='translate(0.479858 0.502869)'
+          fill='url(#paint0_linear_188_447)'
+        />
+        <rect
+          x='11.6542'
+          y='50.0115'
+          width='123'
+          height='156'
+          fill='url(#paint1_linear_188_447)'
+          fill-opacity='0.13'
+        />
+        <defs>
+          <linearGradient
+            id='paint0_linear_188_447'
+            x1='72.6743'
+            y1='0'
+            x2='72.6743'
+            y2='215.509'
+            gradientUnits='userSpaceOnUse'
+          >
+            <stop stop-color='#6CC8C6' />
+            <stop offset='0.5' stop-color='#6CC8C6' />
+            <stop offset='1' stop-color='#6840C0' />
+          </linearGradient>
+          <linearGradient
+            id='paint1_linear_188_447'
+            x1='73.1542'
+            y1='50.0115'
+            x2='73.1542'
+            y2='206.011'
+            gradientUnits='userSpaceOnUse'
+          >
+            <stop stop-color='#FFFBFB' />
+            <stop offset='0.46' stop-color='#6CC8C6' />
+            <stop offset='1' stop-color='#453F3F' />
+          </linearGradient>
+        </defs>
+      </svg>
+    </>
+  );
+}
