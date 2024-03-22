48c8392facd504d2e2255a73952bae848f9ecff6
유호영 <yhu2121@naver.com>
2024-03-22 12:14:23 +0900

    feat(be-refrigerator): #22 Refrigerator 엔티티 생성

---
+++ backend/src/main/java/com/dango/dango/domain/refrigerator/entity/Refrigerator.java
@@ -0,0 +1,38 @@
+package com.dango.dango.domain.refrigerator.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Data
+@Entity
+public class Refrigerator {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    private String nickname;
+
+    @CreationTimestamp
+    @NotNull
+    private LocalDateTime createTime;
+
+    @CreationTimestamp
+    @UpdateTimestamp
+    @NotNull
+    private LocalDateTime updateTime;
+}
