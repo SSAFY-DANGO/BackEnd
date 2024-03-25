74b5808aa7e62df1f8f29191750b4a16445c3c7f
유호영 <yhu2121@naver.com>
2024-03-25 10:24:42 +0900

    fix(be-refrigerator) : register -> 냉장고등록+유저에게 냉장고 등록되도록 변경

--- backend/src/main/java/com/dango/dango/domain/refrigerator/controller/RefrigeratorController.java
+++ backend/src/main/java/com/dango/dango/domain/refrigerator/controller/RefrigeratorController.java
@@ -37,7 +37,7 @@ public class RefrigeratorController {
     }
 
     @PostMapping
-    public ResponseEntity<String> registerRefrigerator(@RequestBody String nickname) {
+    public ResponseEntity<RefrigeratorInfoResponse> registerRefrigerator(@RequestBody String nickname) {
         log.info("냉장고 등록 요청 들어옴");
         // 냉장고를 등록한다.
         // 토큰으로 유저를 찾는다. -> 유저에게 속한 냉장고가 없으면 새로 등록한다.
@@ -46,19 +46,29 @@ public class RefrigeratorController {
         Long refrigeratorId = user.getRefrigeratorId();
 
         refrigeratorService.throwIfRefrigeratorDuplicated(refrigeratorId);
-
         Refrigerator res = refrigeratorService.registerRefrigerator(nickname);
-        return new ResponseEntity<>(res.getNickname() + " 냉장고 등록 완료", HttpStatus.OK);
+
+        // 등록 완료한 냉장고를 유저에게 속하도록
+        user.setRefrigeratorId(res.getId());
+        RefrigeratorInfoResponse response = RefrigeratorInfoResponse.builder()
+                .id(res.getId()).nickname(res.getNickname()).build();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @PutMapping
-    public ResponseEntity<String> editRefrigerator(@RequestBody String nickname) {
+    public ResponseEntity<RefrigeratorInfoResponse> editRefrigerator(@RequestBody String nickname) {
         log.info("냉장고 수정 요청 들어옴");
         // 냉장고를 수정한다.
         // 토큰으로 유저를 찾는다 -> 유저에게 속한 냉장고가 있으면 수정한다.
         User user = userService.findUserByToken();
         Refrigerator res = refrigeratorService.editRefrigerator(user, nickname);
-        return new ResponseEntity<>(res.getNickname() + " 냉장고 등록 완료", HttpStatus.OK);
+
+        RefrigeratorInfoResponse response = RefrigeratorInfoResponse.builder()
+                .id(res.getId()).nickname(res.getNickname()).build();
+
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @DeleteMapping
