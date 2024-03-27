215097649887885a922d97b41906f0175c86386f
jungminlee <leejm234@gmail.com>
2024-03-27 15:07:09 +0900

    Merge branch 'fe-dev' of https://lab.ssafy.com/s10-bigdata-recom-sub2/S10P22A702 into fe-dev

--- dango-project/src/components/Fridge-Inside/FridgeFrame.jsx
+++ dango-project/src/components/Fridge-Inside/FridgeFrame.jsx
@@ -4,9 +4,13 @@ import FoodItem from './FoodItem'
 
 function FridgeFrame() {
 
+    const foodItems = [];
+    for (let i = 0; i < 25; i++) {
+        foodItems.push(<FoodItem nameText="아보카도" />);
+    }
     return (
-    <div className=' w-[80vw] h-[65vh] border-slate-500 border-4 rounded-xl'>
-        <FoodItem nameText="아보카도"/>
+    <div className=' w-[80vw] h-[65vh] border-slate-500 border-4 rounded-xl flex flex-wrap'>
+        {foodItems}
         
     </div>
     );
