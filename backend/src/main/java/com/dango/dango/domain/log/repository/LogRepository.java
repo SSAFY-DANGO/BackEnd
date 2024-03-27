9f3a3dce2499b3e36e8460313bf2cb09b7ce34e7
유호영 <yhu2121@naver.com>
2024-03-27 11:24:00 +0900

    fix: 냉장고에 속한 모든 식재료 로그 조회 repo 추가

--- backend/src/main/java/com/dango/dango/domain/log/repository/LogRepository.java
+++ backend/src/main/java/com/dango/dango/domain/log/repository/LogRepository.java
@@ -2,6 +2,12 @@ package com.dango.dango.domain.log.repository;
 
 import com.dango.dango.domain.log.entity.Log;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface LogRepository extends JpaRepository<Log, Long> {
+    @Query(value = "select l from Log l where l.refrigerator.id = :refrigeratorId")
+    List<Log> findAllByRefrigeratorId(@Param("refrigeratorId") Long refrigeratorId);
 }
