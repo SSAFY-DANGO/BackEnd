ef73decfc5326dfdd8fe272efe0ee7b4fe7688fc
유호영 <yhu2121@naver.com>
2024-03-27 16:39:00 +0900

    refactor(be-refrigerator): 냉장고 비즈니스 로직 서비스 단으로분리

--- backend/src/main/java/com/dango/dango/domain/refrigerator/controller/RefrigeratorController.java
+++ backend/src/main/java/com/dango/dango/domain/refrigerator/controller/RefrigeratorController.java
@@ -2,9 +2,7 @@ package com.dango.dango.domain.refrigerator.controller;
 
 import com.dango.dango.domain.log.entity.Log;
 import com.dango.dango.domain.refrigerator.dto.RefrigeratorInfoResponse;
-import com.dango.dango.domain.refrigerator.entity.Refrigerator;
 import com.dango.dango.domain.refrigerator.service.RefrigeratorService;
-import com.dango.dango.domain.user.entity.User;
 import com.dango.dango.domain.user.service.UserService;
 import com.dango.dango.global.common.response.CustomResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,27 +25,16 @@ public class RefrigeratorController {
     @Operation(summary = "내 냉장고를 조회한다", description = "토큰으로 유저를 조회하고 유저에게 속한 '냉장고'를 리턴합니다.")
     public CustomResponse<RefrigeratorInfoResponse> getRefrigeratorInfo() {
         log.info("냉장고 조회 요청 들어옴");
-        // 내 냉장고 정보를 조회한다.
-        // 토큰으로 유저를 찾고, 그 유저에게 속한 냉장고 정보 조회
-        User user = userService.findUserByToken();
-        Long refrigeratorId = user.getRefrigeratorId();
-        Refrigerator res = refrigeratorService.findRefrigeratorById(refrigeratorId);
-        RefrigeratorInfoResponse response =
-                RefrigeratorInfoResponse.builder()
-                        .id(res.getId())
-                        .nickname(res.getNickname())
-                        .build();
+        RefrigeratorInfoResponse res = refrigeratorService.getRefrigerator();
 
-        return new CustomResponse<>(HttpStatus.OK.value(), response, "내 냉장고 조회 완료");
+        return new CustomResponse<>(HttpStatus.OK.value(), res, "내 냉장고 조회 완료");
     }
 
     @GetMapping("/{refrigeratorId}")
     @Operation(summary = "냉장고 안에 들어있는 품목을 조회한다", description = "식재료 로그 중 냉장고(id)에 속한 모든 물품을 조회합니다.")
     public CustomResponse<List<Log>> getRefrigeratorItems(@PathVariable Long refrigeratorId) {
         log.info("냉장고 품목 조회 들어옴");
-        User user = userService.findUserByToken();
-        Long userRefrigeratorId = user.getRefrigeratorId();
-        List<Log> logList = refrigeratorService.getItems(refrigeratorId, userRefrigeratorId);
+        List<Log> logList = refrigeratorService.getItems(refrigeratorId);
         return new CustomResponse<>(HttpStatus.OK.value(), logList, "냉장고 품목 리스트 조회 완료");
     }
 
@@ -56,19 +43,9 @@ public class RefrigeratorController {
     @Operation(summary = "내 냉장고를 등록한다.", description = "토큰으로 유저를 조회하고, 그 유저에게 냉장고를 등록합니다.")
     public CustomResponse<RefrigeratorInfoResponse> registerRefrigerator(@RequestBody String nickname) {
         log.info("냉장고 등록 요청 들어옴");
-        // 냉장고를 등록한다.
-        // 토큰으로 유저를 찾는다. -> 유저에게 속한 냉장고가 없으면 새로 등록한다.
-        //성공 200 토큰x 400 냉장고 이미 있음 409
-        User user = userService.findUserByToken();
-        Long refrigeratorId = user.getRefrigeratorId();
 
-        refrigeratorService.throwIfRefrigeratorDuplicated(refrigeratorId);
-        Refrigerator res = refrigeratorService.registerRefrigerator(nickname, user);
-
-        RefrigeratorInfoResponse response = RefrigeratorInfoResponse.builder()
-                .id(res.getId()).nickname(res.getNickname()).build();
-
-        return new CustomResponse<>(HttpStatus.OK.value(), response, "내 냉장고 등록 완료");
+        RefrigeratorInfoResponse res = refrigeratorService.registerRefrigerator(nickname);
+        return new CustomResponse<>(HttpStatus.OK.value(), res, "내 냉장고 등록 완료");
     }
 
     @PutMapping
@@ -77,14 +54,8 @@ public class RefrigeratorController {
         log.info("냉장고 수정 요청 들어옴");
         // 냉장고를 수정한다.
         // 토큰으로 유저를 찾는다 -> 유저에게 속한 냉장고가 있으면 수정한다.
-        User user = userService.findUserByToken();
-        Refrigerator res = refrigeratorService.editRefrigerator(user, nickname);
-
-        RefrigeratorInfoResponse response = RefrigeratorInfoResponse.builder()
-                .id(res.getId()).nickname(res.getNickname()).build();
-
-
-        return new CustomResponse<>(HttpStatus.OK.value(), response, "내 냉장고 수정 완료");
+        RefrigeratorInfoResponse res = refrigeratorService.editRefrigerator(nickname);
+        return new CustomResponse<>(HttpStatus.OK.value(), res, "내 냉장고 수정 완료");
     }
 
     @DeleteMapping
@@ -93,10 +64,7 @@ public class RefrigeratorController {
         log.info("냉장고 삭제 요청 들어옴");
         // 냉장고를 삭제한다.
         // 토큰으로 유저 찾기 -> 등록된 냉장고 삭제
-        User user = userService.findUserByToken();
-        Long refrigeratorId = user.getRefrigeratorId();
-        refrigeratorService.throwIfRefrigeratorNotExist(refrigeratorId);
-        refrigeratorService.deleteRefrigerator(refrigeratorId);
-        return new CustomResponse<>(HttpStatus.OK.value(), refrigeratorId, refrigeratorId + "번 냉장고 삭제 완료");
+        Long deleted = refrigeratorService.deleteRefrigerator();
+        return new CustomResponse<>(HttpStatus.OK.value(), deleted, deleted + "번 냉장고 삭제 완료");
     }
 }
