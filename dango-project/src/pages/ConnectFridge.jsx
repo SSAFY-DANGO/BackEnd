f424ac0d0bdda5e193ca55b4a1c19396804bec4b
Monami123321 <mmonami123321@gmail.com>
2024-04-01 23:43:38 +0900

    refactoring(fe-refrigerator): 냉장고 관련 API 분리, 로직 추가, 예외 추가
    - 냉장고 관련 CRUD API 완전분리
    - token, nonetoken axios instance 분리, 인터셉터 추가
    - tokenapi는 localstorage 참조하여 header 에 jwt를 추가하도록 로직 추가
    - 냉장고 등록 예외 처리 추가
    - swagger기준 냉장고 API 작성

--- dango-project/src/pages/ConnectFridge.jsx
+++ dango-project/src/pages/ConnectFridge.jsx
@@ -1,39 +1,51 @@
-import React from 'react';
-import {useState} from 'react';
-import { useNavigate } from 'react-router-dom';
-import { postRefrigerator } from '../api/Api'
+import React from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { refrigeratorAPI } from "../api/refrigeratorAPI";
 
 export default function ConnectFridge() {
-
   const navigate = useNavigate();
-  const [fridgeKey, setFridgeKey] = useState('');
+  const [fridgeKey, setFridgeKey] = useState("");
 
   const handleConnectKey = async () => {
-
-    try {
-      console.log(fridgeKey)
-      const response = await postRefrigerator(fridgeKey);
-      console.log('냉장고 연결 성공', response)
-      navigate('/fridge-exterior')
-    } catch (error) {
-      alert('냉장고 연결에 실패했습니다.')
-      console.log('냉장고 연결 실패', error);
+    if (!fridgeKey) {
+      alert("냉장고 키를 입력하세요.");
+      return;
     }
-  }
+    try {
+      const res = await refrigeratorAPI.register(fridgeKey);
+      if (res.data.status === 200) {
+        alert("냉장고 연결 성공");
+        navigate("/fridge-exterior");
+      } else {
+        alert(res.data.message);
+      }
+    } catch (error) {
+      alert("냉장고 연결 요청이 실패했습니다.");
+      console.log("냉장고 연결 요청 실패", error);
+    }
+  };
   return (
     <>
-      <div className='landing-background flex flex-col justify-center items-center h-screen'>
-        <div className='text-5xl font-bold pb-10 font-title'>냉장고를 연결해주세요</div>
-        <div className='m-4 w-3/4 flex justify-center flex-col'>
-        <div className='mb-[1vh]'>냉장고Key</div>
-            <input
-              placeholder='48sdfuwjcv@#'
-              className='rounded-lg p-3 w-[70vw]'
-              value={fridgeKey}
-              onChange={(e) => setFridgeKey(e.target.value)}
-            />
-           
-        <button className='long-thick-button w-[70vw]' onClick={handleConnectKey}> 냉장고 Key 연결하기 </button>    
+      <div className="landing-background flex flex-col justify-center items-center h-screen">
+        <div className="text-5xl font-bold pb-10 font-title">
+          냉장고를 연결해주세요
+        </div>
+        <div className="m-4 w-3/4 flex justify-center flex-col">
+          <div className="mb-[1vh]">냉장고Key</div>
+          <input
+            placeholder="48sdfuwjcv@#"
+            className="rounded-lg p-3 w-[70vw]"
+            value={fridgeKey}
+            onChange={(e) => setFridgeKey(e.target.value)}
+          />
+
+          <button
+            className="long-thick-button w-[70vw]"
+            onClick={handleConnectKey}
+          >
+            냉장고 Key 연결하기
+          </button>
         </div>
       </div>
     </>
