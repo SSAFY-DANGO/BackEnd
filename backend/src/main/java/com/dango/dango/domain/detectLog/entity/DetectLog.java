2c4cfc09d8104e093857da69e01b6af04e6037b0
On Yoo <dhsdb02@gmail.com>
2024-04-01 15:59:52 +0900

    feat(detect-log): #41 detect log 엔티티 추가

---
+++ backend/src/main/java/com/dango/dango/domain/detectLog/entity/DetectLog.java
@@ -0,0 +1,44 @@
+package com.dango.dango.domain.detectLog.entity;
+
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.hibernate.annotations.Type;
+import io.hypersistence.utils.hibernate.type.json.JsonType;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "detect_log")
+public class DetectLog {
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	Long id;
+
+	@Column(name = "input_time")
+	LocalDateTime inputTime;
+
+	@Type(JsonType.class)
+	@Column(columnDefinition = "data")
+	private Map<String,Integer> data = new HashMap<>();
+
+	@Column(columnDefinition = "input_type")
+	private String inputType;
+
+	@Column(columnDefinition = "device")
+	private String device;
+}
