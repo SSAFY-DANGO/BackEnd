7873e80d20b603557f515fe5e6ccbaacf898cc0c
유호영 <yhu2121@naver.com>
2024-04-02 15:44:25 +0900

    fix(fe-refrigerator): 냉장고 겉면 import수정

--- dango-project/src/pages/FridgeExterior.jsx
+++ dango-project/src/pages/FridgeExterior.jsx
@@ -1,16 +1,40 @@
-import React from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
-import Header from '../components/Header';
-import Footer from '../components/Footer';
-import FridgeSVG from '../components/FridgeSVG';
-import { useState } from 'react';
-import AlertModal from '../components/Fridge-Exterior/AlertModal';
-import { getRefrigeratorOld, getRefrigeratorOpen } from '../api/Api';
-import { useEffect } from 'react';
-import { useRecoilValue } from 'recoil';
-import { loginUserState, userState } from '../recoil/atoms/userState';
-import AlertButton from '../components/Fridge-Exterior/AlertButton';
-import { refrigeratorAPI } from '../api/refrigeratorAPI';
+import React from "react";
+import { useNavigate, useLocation } from "react-router-dom";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import FridgeSVG from "../components/FridgeSVG";
+import { useState } from "react";
+import AlertModal from "../components/Fridge-Exterior/AlertModal";
+import { getRefrigeratorOld, getRefrigeratorOpen } from "../api/Api";
+import { useEffect } from "react";
+import AlertButton from "../components/Fridge-Exterior/AlertButton";
+import { useRecoilValue, useRecoilState } from "recoil";
+import { loginUserState, userState } from "../recoil/atoms/userState";
+import { foodOldItemsState } from "../recoil/atoms/userState";
+import { refrigeratorAPI } from "../api/refrigeratorAPI";
+import 아보카도 from "../assets/imgs/groceries/아보카도.png";
+import 감자 from "../assets/imgs/groceries/감자.png";
+import 고추 from "../assets/imgs/groceries/고추.png";
+import 귤 from "../assets/imgs/groceries/귤.png";
+import 당근 from "../assets/imgs/groceries/당근.png";
+import 대파 from "../assets/imgs/groceries/대파.png";
+import 돼지고기 from "../assets/imgs/groceries/돼지고기.png";
+import 딸기 from "../assets/imgs/groceries/딸기.png";
+import 마늘 from "../assets/imgs/groceries/마늘.png";
+import 무 from "../assets/imgs/groceries/무.png";
+import 방울토마토 from "../assets/imgs/groceries/방울토마토.png";
+import 배 from "../assets/imgs/groceries/배.png";
+import 배추 from "../assets/imgs/groceries/배추.png";
+import 버섯 from "../assets/imgs/groceries/버섯.png";
+import 복숭아 from "../assets/imgs/groceries/복숭아.png";
+import 사과 from "../assets/imgs/groceries/사과.png";
+import 슬라이스치즈 from "../assets/imgs/groceries/슬라이스 치즈.png";
+import 애호박 from "../assets/imgs/groceries/애호박.png";
+import 양배추 from "../assets/imgs/groceries/양배추.png";
+import 양파 from "../assets/imgs/groceries/양파.png";
+import 토마토 from "../assets/imgs/groceries/토마토.png";
+import 포도 from "../assets/imgs/groceries/포도.png";
+import 파프리카 from "../assets/imgs/groceries/파프리카.png";
 
 function FridgeExterior() {
   useEffect(() => {
@@ -47,48 +71,48 @@ function FridgeExterior() {
   const [foodOldItems, setFoodOldItems] = useRecoilState(foodOldItemsState);
 
   const loginUser = useRecoilValue(loginUserState);
-  
+
   const { nickname, refrigeratorNickname } = useRecoilValue(loginUserState);
 
   const getRefrigeratorInfo = async () => {
     try {
       const response = await getRefrigeratorOld(
-        refrigeratorNickname, loginUser.accessToken
+        refrigeratorNickname,
+        loginUser.accessToken
       );
-      console.log('냉장고 조회 성공', response);
+      console.log("냉장고 조회 성공", response);
       console.log(response.data.map((item) => item.name));
       setFoodOldItems(response.data.map((item) => item.name));
-      localStorage.setItem('id', response.data.id);
+      localStorage.setItem("id", response.data.id);
     } catch (error) {
-      console.log('냉장고 조회 실패', error);
+      console.log("냉장고 조회 실패", error);
     }
   };
 
-  const getRefrigeratorOpen = async() => {
-    try{
+  const getRefrigeratorOpen = async () => {
+    try {
       const response = await refrigeratorAPI.isDoorOpen();
       const isOpen = response.data.data;
       setIsRefrigeratorOpen(isOpen);
-    }catch(error){
-      console.log('냉장고 조회 실패', error);
+    } catch (error) {
+      console.log("냉장고 조회 실패", error);
     }
-  }
+  };
 
-  
   const navigate = useNavigate();
   const location = useLocation();
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isRefrigeratorOpen,setIsRefrigeratorOpen] = useState(false);
+  const [isRefrigeratorOpen, setIsRefrigeratorOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
 
   const goToInside = () => {
-    navigate('/fridge-inside');
+    navigate("/fridge-inside");
   };
 
   const goToMypage = () => {
-    navigate('/mypage');
+    navigate("/mypage");
   };
 
   const handleAlarmClick = () => {
@@ -100,7 +124,7 @@ function FridgeExterior() {
   const selectedImages = foodOldItems.map((item) => {
     selectedImage = imageMap[item];
     if (!selectedImage) {
-      selectedImage = imageMap['랜덤'];
+      selectedImage = imageMap["랜덤"];
     }
     return selectedImage;
   });
@@ -114,8 +138,8 @@ function FridgeExterior() {
     const hours = now.getHours();
     const minutes = now.getMinutes();
     // 시간과 분을 두 자리 숫자로 포맷팅
-    const formattedTime = `${hours < 10 ? '0' + hours : hours}:${
-      minutes < 10 ? '0' + minutes : minutes
+    const formattedTime = `${hours < 10 ? "0" + hours : hours}:${
+      minutes < 10 ? "0" + minutes : minutes
     }`;
     return formattedTime;
   };
@@ -125,25 +149,25 @@ function FridgeExterior() {
 
   return (
     <>
-      <Header text={'예은이의 냉장고'} />
-      
-      <div className='relative h-[590px] pt-[50px]'>
+      <Header text={"예은이의 냉장고"} />
+
+      <div className="relative h-[590px] pt-[50px]">
         {/* relative h-screen */}
         {/* absolute left-1/2 transform -translate-x-1/2 translate-y-1/2 */}
-        <div className='absolute left-1/2 transform -translate-x-1/2 '>
-        <div>
-        <AlertButton isOpen = {isRefrigeratorOpen}/>
-        </div>
-         <div>
-         <FridgeSVG
-            time={currentTime}
-            onAlarmClick={handleAlarmClick}
-            onFridgeClick={goToInside}
-            selectedImages={selectedImages}
-            // selectedImage={selectedImage}
-            // foodOldItems={foodOldItems}
-          />
-         </div>  
+        <div className="absolute left-1/2 transform -translate-x-1/2 ">
+          <div>
+            <AlertButton isOpen={isRefrigeratorOpen} />
+          </div>
+          <div>
+            <FridgeSVG
+              time={currentTime}
+              onAlarmClick={handleAlarmClick}
+              onFridgeClick={goToInside}
+              selectedImages={selectedImages}
+              // selectedImage={selectedImage}
+              // foodOldItems={foodOldItems}
+            />
+          </div>
         </div>
         <AlertModal
           isOpen={isModalOpen}
