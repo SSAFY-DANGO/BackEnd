329657500bb45a7de1185e5e04d94c31c2a15148
On Yoo <dhsdb02@gmail.com>
2024-04-02 13:34:25 +0900

    feat(door): #42 문 리포지토리 쿼리 수정

--- backend/src/main/java/com/dango/dango/domain/door/repository/DoorRepository.java
+++ backend/src/main/java/com/dango/dango/domain/door/repository/DoorRepository.java
@@ -3,13 +3,16 @@ package com.dango.dango.domain.door.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import com.dango.dango.domain.door.entity.Door;
 
+@Repository
 public interface DoorRepository extends JpaRepository<Door, Long> {
-	@Query(value = "select d from Door d where d.deviceNickname = :deviceNickName and d.isOpen = :isOpen order by d.time desc limit 1")
-	Door findLatestItem(@Param("deviceNickname") String deviceNickName, @Param("date") Boolean isOpen);
+	@Query(value = "select d from Door d where d.deviceNickname = :deviceNickname and d.isOpen = :isOpen order by d.time desc limit 1")
+	Door findLatestItem(@Param("deviceNickname") String deviceNickname, @Param("isOpen") Boolean isOpen);
 
 	@Query(value = "select d.isOpen from Door d where d.deviceNickname = :deviceNickname order by d.time desc limit 1")
-	Boolean findLatestItemByNickname(@Param("deviceNickname") String deviceNickname);
+	Boolean findLatestItemByNickname(@Param("deviceNickname")String deviceNickname);
+
 }
