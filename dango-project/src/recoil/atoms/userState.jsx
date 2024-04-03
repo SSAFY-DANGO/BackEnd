65becae2298be0d40cb5690c86fe2693c73af47e
jungminlee <leejm234@gmail.com>
2024-04-03 17:03:02 +0900

    feat: recoil-persist 추가

--- dango-project/src/recoil/atoms/userState.jsx
+++ dango-project/src/recoil/atoms/userState.jsx
@@ -1,9 +1,14 @@
+
 import { atom } from 'recoil';
+import { recoilPersist } from 'recoil-persist';
+
 const loginUser = JSON.parse(localStorage.getItem('loginUser'));
+const { persistAtom } = recoilPersist();
 
 export const userState = atom({
   key: 'userState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const loginUserState = atom({
@@ -14,22 +19,27 @@ export const loginUserState = atom({
     accessToken: loginUser ? loginUser.accessToken : '', // AccessToken 조회
     refreshToken: loginUser ? loginUser.refreshToken : '', // RefreshToken 조회
   },
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const foodItemsState = atom({
   key: 'foodItemsState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 export const trashItemsState = atom({
   key: 'trashItemsState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
 export const userInputState = atom({
   key: 'userInputState',
   default: '',
+  effects_UNSTABLE: [persistAtom],
 });
 
 export const foodOldItemsState = atom({
   key: 'foodOldItemsState',
   default: [],
+  effects_UNSTABLE: [persistAtom],
 });
