842f572afde67ea29bea13e3aa36ce5b43d6ef17
전예은 <ye_eun12@naver.com>
2024-04-03 13:36:44 +0900

    feat: Header Component 클릭 시 cursor point 변경

--- dango-project/src/components/Header.jsx
+++ dango-project/src/components/Header.jsx
@@ -1,28 +1,36 @@
 import React from 'react';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { foodOldItemsState, loginUserState } from '../recoil/atoms/userState';
+import { useNavigate } from 'react-router-dom';
 
 function Header({ text }) {
   const [foodOldItems, setFoodOldItems] = useRecoilState(foodOldItemsState);
 
+  const navigate = useNavigate();
   const { nickname } = useRecoilValue(loginUserState);
+
+  const goToExterior = () => {
+    navigate('/fridge-exterior');
+  };
   return (
     <>
-      {foodOldItems.length === 0 ? (
-        <header className='self-stretch rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(57,_240,_240,_0.5)_69%,_rgba(57,_240,_240,_1))] flex flex-row items-start justify-center pt-[29px] px-5 pb-7 box-border top-[0] z-[99] whitespace-nowrap max-w-full text-left text-6xl text-black1'>
-          <div className='h-[87px] w-[430px] relative rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] hidden max-w-full' />
-          <h3 className='m-0 relative text-inherit tracking-[-0.04em] font-small font-inherit z-[1] font-aggroM'>
-            {nickname}의 냉장고
-          </h3>
-        </header>
-      ) : (
-        <header className='self-stretch rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] flex flex-row items-start justify-center pt-[29px] px-5 pb-7 box-border top-[0] z-[99] whitespace-nowrap max-w-full text-left text-6xl text-black1'>
-          <div className='h-[87px] w-[430px] relative rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] hidden max-w-full' />
-          <h3 className='m-0 relative text-inherit tracking-[-0.04em] font-small font-inherit z-[1] font-aggroM'>
-            {nickname}의 냉장고
-          </h3>
-        </header>
-      )}
+      <div onClick={goToExterior}>
+        {foodOldItems.length === 0 ? (
+          <header className='cursor-pointer self-stretch rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(57,_240,_240,_0.5)_69%,_rgba(57,_240,_240,_1))] flex flex-row items-start justify-center pt-[29px] px-5 pb-7 box-border top-[0] z-[99] whitespace-nowrap max-w-full text-left text-6xl text-black1'>
+            <div className='h-[87px] w-[430px] relative rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] hidden max-w-full' />
+            <h3 className='m-0 relative text-inherit tracking-[-0.04em] font-small font-inherit z-[1] font-aggroM'>
+              {nickname}의 냉장고
+            </h3>
+          </header>
+        ) : (
+          <header className='cursor-pointer self-stretch rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] flex flex-row items-start justify-center pt-[29px] px-5 pb-7 box-border top-[0] z-[99] whitespace-nowrap max-w-full text-left text-6xl text-black1'>
+            <div className='h-[87px] w-[430px] relative rounded-t-none rounded-b-xl [background:linear-gradient(180deg,_rgba(202,_249,_249,_0.42),_rgba(202,_249,_249,_0.42)_0.01%,_rgba(203,_180,_202,_0.28)_69%,_rgba(203,_149,_182,_0.21))] hidden max-w-full' />
+            <h3 className='m-0 relative text-inherit tracking-[-0.04em] font-small font-inherit z-[1] font-aggroM'>
+              {nickname}의 냉장고
+            </h3>
+          </header>
+        )}
+      </div>
     </>
   );
 }
